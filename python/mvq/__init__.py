d4088fb3660dfc80d43673fbe43298bec79bab24
Hayk Martirosyan <hayk.mart@gmail.com>
2015-02-21 16:59:02 -0800

    Add requirements, test images

--- python/mvq/__init__.py
+++ python/mvq/__init__.py
@@ -12,6 +12,11 @@ __license__ = 'GPLv2'
 import os
 path = os.path.dirname(os.path.realpath(__file__))
 
+# Paths to data directories
+data_path = os.path.join(os.path.dirname(os.path.dirname(path)), 'data')
+images_path = os.path.join(data_path, 'images')
+videos_path = os.path.join(data_path, 'videos')
+
 # Set up logging
 import logging
 logging.basicConfig(format='%(message)s', level=logging.DEBUG)
--- python/mvq/test_image.py
+++ python/mvq/test_image.py
@@ -1,5 +1,5 @@
 """
-Playing around with OpenCV.
+Test to load and display an image.
 
 """
 
@@ -7,17 +7,22 @@ import os
 import sys
 import numpy as np
 import cv2
+from mvq import images_path
 
 
 def load_image(image_file):
 
     # Load a color image
     img = cv2.imread(image_file, cv2.IMREAD_COLOR)
+
+    cv2.rectangle(img, (100, 300), (200, 400), (0, 255, 255), 2)
+
     cv2.imshow('Picture of a road', img)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 
-    filename = '../../data/images/road.jpg'
+    filename = os.path.join(images_path, 'road.jpg')
     load_image(filename)
