42038b28d56fb203efb61424798b00a40a6e2fb8
Hayk Martirosyan <hayk.mart@gmail.com>
2015-02-23 13:54:54 -0800

    Better lane marker line detection

--- python/mvq/filters/lane_marker.py
+++ python/mvq/filters/lane_marker.py
@@ -3,9 +3,10 @@
 """
 
 import numpy as np
+import cv2
 
 
-def lane_marker_filter(img, tau, threshold=130):
+def lane_marker_filter(img, tau):
     """
     Lane marking detector - filter for detecting lane markings.
 
@@ -25,14 +26,22 @@ def lane_marker_filter(img, tau, threshold=130):
     img_out = np.zeros(img_in.shape)
     img_out[:, tau:-tau] = img_in[:, tau:-tau] * 2
 
-    img_out[:, tau:-tau] -= img_left
-    img_out[:, tau:-tau] -= img_right
+    img_out[:, tau:-tau] -= img_left + img_right
     img_out[:, tau:-tau] -= np.abs(img_left - img_right)
+
+    # Cut off the top third
+    cutoff_min_y = int(img_out.shape[0] * 1/3)
+    img_out[0:cutoff_min_y, :] = 0
 
     # Clamp image
     img_out = np.clip(img_out, 0, 255)
 
-    img_thresholded = np.zeros(img_out.shape, np.uint8)
-    img_thresholded[img_out >= threshold] = 1
+    # Otsu thresholding
+    img_out = np.uint8(img_out)
+    ret3, img_out = cv2.threshold(img_out, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    return img_thresholded
+    # img_thresholded = np.zeros(img_out.shape, np.uint8)
+    # img_thresholded[img_out >= threshold] = 1
+    # img_out = img_thresholded
+
+    return img_out
--- python/mvq/reconstruction.py
+++ python/mvq/reconstruction.py
@@ -53,31 +53,59 @@ def find_vanishing_point(img):
     """
 
     # Filter to find the lane markers
-    filtered_img = lane_marker_filter(img, tau=15, threshold=130)
+    filtered_img = lane_marker_filter(img, tau=15)
+
+    # Erode the smaller features away, make lane lines thinner
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    filtered_img = cv2.erode(filtered_img, kernel=kernel)
+
+    # filtered_img = cv2.Laplacian(img, cv2.CV_8U)
+    # filtered_img = np.uint8(np.abs(filtered_img))
+    # filtered_img = cv2.Canny(filtered_img, 300, 400, apertureSize=3, L2gradient=True)
+    # filtered_img = cv2.GaussianBlur(filtered_img, (5, 5), 0)
+    # filtered_img = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+    # filtered_img = cv2.Laplacian(filtered_img, cv2.CV_64F)
+    # filtered_img = np.uint8(np.abs(filtered_img))
+    # filtered_img = cv2.morphologyEx(filtered_img, cv2.MORPH_OPEN, kernel)
+    # ret3, filtered_img = cv2.threshold(filtered_img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # filtered_img = cv2.dilate(filtered_img, kernel=kernel)
+    # filtered_img = cv2.Laplacian(filtered_img, cv2.CV_64F, scale=300)
+    # filtered_img = np.uint8(np.abs(filtered_img))
 
     # Calculate hough lines
-    lines = cv2.HoughLines(image=filtered_img, rho=20, theta=.01, threshold=700)
+    # lines = cv2.HoughLines(image=filtered_img, rho=10, theta=np.pi*10/360, threshold=140, srn=10, stn=10)
+    lines = cv2.HoughLinesP(image=filtered_img, rho=1, theta=np.pi*1/180, threshold=80, minLineLength=170, maxLineGap=120)
+
+    if lines is None:
+        lines = []
 
     # Create a copy of the input to draw lines on
     img_copy = np.array(img)
+    img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2BGR)
+
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        p1 = (x1, y1)
+        p2 = (x2, y2)
+        cv2.line(img_copy, p1, p2, (255, 255, 0), 2)
 
     # Draw the hough lines
-    for line in lines:
-
-        # Calculate line in cartesian
-        rho, theta = line[0]
-        a, b = np.cos(theta), np.sin(theta)
-        x0, y0 = a * rho, b * rho
-
-        # Calculate points on line
-        p1 = x0 + 1000 * (-b), y0 + 1000 * a
-        p2 = x0 - 1000 * (-b), y0 - 1000 * a
-
-        # Round to integer
-        p1 = [int(p) for p in p1]
-        p2 = [int(p) for p in p2]
-
-        cv2.line(img_copy, tuple(p1), tuple(p2), (255, 0, 0), 1)
+    # for line in lines:
+    #
+    #     # Calculate line in cartesian
+    #     rho, theta = line[0]
+    #     a, b = np.cos(theta), np.sin(theta)
+    #     x0, y0 = a * rho, b * rho
+    #
+    #     # Calculate points on line
+    #     p1 = x0 + 1000 * (-b), y0 + 1000 * a
+    #     p2 = x0 - 1000 * (-b), y0 - 1000 * a
+    #
+    #     # Round to integer
+    #     p1 = [int(p) for p in p1]
+    #     p2 = [int(p) for p in p2]
+    #
+    #     cv2.line(img_copy, tuple(p1), tuple(p2), (255, 0, 0), 1)
 
     # Plot
     plt.figure()
@@ -97,7 +125,7 @@ if __name__ == '__main__':
 
     find_vanishing_point(imgL)
 
-    plot_disparity_map(imgL, imgR)
-    plot_edges(imgL)
+    # plot_disparity_map(imgL, imgR)
+    # plot_edges(imgL)
 
     plt.show()
